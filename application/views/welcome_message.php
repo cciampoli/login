993734dbc535608eb186ef17a39aee14e058ebcb
unknown <cciampoli@transactionwireless.com>
2012-12-20 12:35:29 -0800

    Changes to Welcome Message View

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,11 +67,20 @@
 <body>
 
 <div id="container">
-	<h1>ChrisChross Login</h1>
-
-	<div id="body">
-		
-	</div>
+	<div align="center">
+<h1>Login</h1>
+<table>
+<?php echo form_open('admin/login'); ?>
+<p><?=form_label('Email Address', 'email_address'); ?></p>
+<p><?=form_input('email_address', set_value('email_address'), 'id="email_address"'); ?></p>
+<p><?=form_label('Password', 'password'); ?></p>
+<p><?=form_password('password', '', 'id="password"'); ?></p>
+<p><?=form_submit('submit', 'Submit!'); ?></p>
+<?php echo form_close(); ?>
+<a href="<?=site_url('admin/register'); ?>">Not a registered user?  Click here to register now!</a>
+<div class="errors"><?=validation_errors(); ?></div>
+</table>
+</div>
 
 	<p class="footer">ChrisChross Spellbook</p>
 </div>
