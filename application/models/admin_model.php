cddfe6ed981d5c13827d55a6b8f9066ae8a0755a
unknown <cciampoli@transactionwireless.com>
2012-12-21 14:31:17 -0800

    changes

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -22,6 +22,16 @@ class Admin_model extends CI_Model {
 		return false;
 	}
 	
+	public function register_user($first_name,$last_name,$email_address,$password)
+	{
+		$q = $this
+				->db
+				->set('first_name',$first_name)
+				->set('last_name',$last_name)
+				->set('email_address',$email_address)
+				->set('password',sha1($password))
+				->insert('users');
+	}	
 }
 
 ?>
