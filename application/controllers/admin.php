e36ff1fb8fa0d2b82aa8f7de1658e66f3216749c
Chris Ciampoli <chrisciampoli@gmail.com>
2012-12-24 23:36:27 -0800

    Changes to email system

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -5,19 +5,19 @@ class Admin extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		session_start();
-		$this -> load -> library('form_validation');
+		$this->load->library('form_validation');
 	}
 
 	function index() {
-		$this -> load -> view('login_view');
+		$this->load->view('login_view');
 	}
 
 	public function login() {
 		
-		$this -> form_validation ->set_rules('email_address', 'Email Address', 'required|valid_email');
-		$this -> form_validation -> set_rules('password', 'Password', 'required|min_length[4]');
+		$this->form_validation->set_rules('email_address', 'Email Address', 'required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[4]');
 
-		if ($this -> form_validation -> run() !== FALSE) {
+		if ($this->form_validation->run() !== FALSE) {
 			// Then query the DB because the form was filled out correctly
 			$this->load->model('admin_model');
 			$res = $this
